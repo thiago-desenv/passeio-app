247c15fbbd31bac3a4b95d69291015820ff2f3ad
thiago_osim <thiago_osim@hotmail.com>
2025-10-06 19:47:52 -0300

    Realizado a configuração da rota do projeto

--- src/app/template/template-routing-module.ts
+++ src/app/template/template-routing-module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Layout } from './layout/layout';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: Layout
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
