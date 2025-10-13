b3054f85b3649ebee661a6963363b1d257120011
thiago_osim <thiago_osim@hotmail.com>
2025-10-12 21:21:21 -0300

    Criado o componente da aba de galeria

--- src/app/galeria/galeria-module.ts
+++ src/app/galeria/galeria-module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GaleriaRoutingModule } from './galeria-routing-module';
+import { Galeria } from './galeria/galeria';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Galeria
+  ],
   imports: [
     CommonModule,
     GaleriaRoutingModule
