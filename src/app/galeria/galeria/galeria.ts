0263e2442472f442706277c89929bc48c2a4b303
thiago_osim <thiago_osim@hotmail.com>
2025-10-12 23:01:36 -0300

    Implementado a busca de dados e carregamento nas variáveis de lugares e categorias para a aba Galeria

--- src/app/galeria/galeria/galeria.ts
+++ src/app/galeria/galeria/galeria.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Lugar } from '../../lugares/lugar';
+import { Categoria } from '../../categorias/categoria';
+import { LugarService } from '../../lugares/services/lugar';
+import { CategoriaService } from '../../categorias/services/categoria';
 
 @Component({
   selector: 'app-galeria',
@@ -6,6 +10,24 @@ import { Component } from '@angular/core';
   templateUrl: './galeria.html',
   styleUrl: './galeria.scss'
 })
-export class Galeria {
+export class Galeria implements OnInit {
+  constructor(
+    private serviceLugares: LugarService,
+    private serviceCategoria: CategoriaService
+  ) { }
 
+  ngOnInit(): void {
+    this.serviceLugares.obter().subscribe({
+      next: lugares => this.lugares = lugares,
+      error: erro => console.log('Ocorreu um erro ao obter os lugares', erro)
+    });
+
+    this.serviceCategoria.obter().subscribe({
+      next: categorias => this.categorias = categorias,
+      error: erro => console.log('Ocorreu um erro ao obter as categorias', erro)
+    });
+  }
+
+  lugares: Lugar[] = [];
+  categorias: Categoria[] = [];
 }
