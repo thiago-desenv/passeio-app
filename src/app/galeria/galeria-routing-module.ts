d4ffe2aa48c4fcb6c0f01ff0e8bd749b5a6bc9e1
thiago_osim <thiago_osim@hotmail.com>
2025-10-12 21:28:18 -0300

    Configurado o módulo de galeria no módulo do template do projeto

--- src/app/galeria/galeria-routing-module.ts
+++ src/app/galeria/galeria-routing-module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Galeria } from './galeria/galeria';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: Galeria,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
