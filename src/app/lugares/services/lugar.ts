7ce38b3429d397b4485cf0b606ccd5376dd0a254
thiago_osim <thiago_osim@hotmail.com>
2025-10-14 18:36:44 -0300

    Criado environments para captura de valores

--- src/app/lugares/services/lugar.ts
+++ src/app/lugares/services/lugar.ts
@@ -2,19 +2,22 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Lugar } from '../lugar';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LugarService {
+  apiUrl: string = environment.apiUrl + 'lugares';
+
   constructor(private http: HttpClient) { }
 
   salvar(lugar: Lugar): Observable<Lugar> {
-    return this.http.post<Lugar>('http://localhost:3000/lugares', lugar);
+    return this.http.post<Lugar>(this.apiUrl, lugar);
   }
 
   obter(): Observable<Lugar[]> {
-    return this.http.get<Lugar[]>('http://localhost:3000/lugares');
+    return this.http.get<Lugar[]>(this.apiUrl);
   }
 
   obterLugaresFiltrado(nome: string, categoria: string): Observable<Lugar[]> {
@@ -27,9 +30,7 @@ export class LugarService {
       params = params.set('categoria', categoria);
     }
 
-    console.log('Parâmetros requisição', params);
-
-    return this.http.get<Lugar[]>('http://localhost:3000/lugares', {
+    return this.http.get<Lugar[]>(this.apiUrl, {
       params: params
     });
   }
